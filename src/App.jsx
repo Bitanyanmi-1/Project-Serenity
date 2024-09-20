44f0913bae71881aada6c64eae1b05cb72eebc76
Justice <justicenb2@gmail.com>
2024-09-20 19:22:57 +0000

    Fronend of the project, first commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+// import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Dashboard from "./Dashboard.jsx";
+import Courses from "./Courses.jsx";
+import Assignments from "./Assignments.jsx";
+import Forum from "./Forum.jsx";
+import Profile from "./Profile.jsx";
+import Logins from "./Logins.jsx";
+import Calendar from "./Calendar.jsx";
+// import TeamLogo from "./Team Logo.png";
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="app-container">
+        <nav className="sidebar">
+          <h2>Student Portal</h2>
+          <ul>
+            <li>
+              <Link to="/" className="nav-link">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/courses" className="nav-link">Courses</Link>
+            </li>
+            <li>
+              <Link to="/assignments" className="nav-link">Assignments</Link>
+            </li>
+            <li>
+              <Link to="/forum" className="nav-link">Forum</Link>
+            </li>
+            <li>
+              <Link to="/profile" className="nav-link">Profile</Link>
+            </li>
+            <li>
+              <Link to="/logins" className="nav-link">Login</Link>
+            </li>
+            <li>
+              <Link to="/calendar" className="nav-link">Calendar</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/courses" element={<Courses />} />
+            <Route path="/assignments" element={<Assignments />} />
+            <Route path="/forum" element={<Forum />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/logins" element={<Logins />} />
+            <Route path="/calendar" element={<Calendar />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
