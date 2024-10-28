4a6b007d361288c3f2e7df42c546da72e3f6083f
N-nyiimock Bitanyanmi <118021911+Bitanyanmi-1@users.noreply.github.com>
2024-10-28 07:09:46 +0000

    Added some files and configured routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
+import Home from "./components/Home";
+import Courses from "./components/Courses"; // Make sure to create these components
+import Assignments from "./components/Assignments";
+import Timetable from "./components/Timetable";
+import Forum from "./components/Forum";
+import Settings from "./components/Settings";
 import "./css/App.css";
 
 function App() {
   return (
-    <>
-      <Sidebar />
-    </>
+    <Router>
+      <div className="app-container">
+        <Sidebar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/courses" element={<Courses />} />
+            <Route path="/assignments" element={<Assignments />} />
+            <Route path="/timetable" element={<Timetable />} />
+            <Route path="/forum" element={<Forum />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
